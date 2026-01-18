6f13e2f216fd7eee1aca5cfbd0ba7187d95ec963
CODERAKS11 <147265113+CODERAKS11@users.noreply.github.com>
2026-01-18 23:32:59 +0530

    Enhance array.js with flattening and Array methods
    Added examples demonstrating array flattening and Array methods.

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -15,17 +15,32 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
+// array.flat(depth)
+// depth = how many levels deep you want to flatten
+// Default depth = 1
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
 
-
+// "Hitesh" is a string primitive, not an array.
+// Array.isArray() is a strict check → only returns true for real arrays ([]).
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
 
+console.log(Array.from("Hitesh")) //['H', 'i', 't', 'e', 's', 'h']
+//Array.from() works on iterables
+// A string is iterable (character by character)
+//Array.from("12345", Number) // [1, 2, 3, 4, 5]
+console.log(Array.from({name: "hitesh"})) //  output : []
+// Why does this NOT throw an error?
+// Because Array.from() expects one of two things:
+// An iterable (like string, array, Set, Map)
+// OR an array-like object (must have length property)
+
+//Array.from({ length: 3 })
+// [undefined, undefined, undefined]
 let score1 = 100
 let score2 = 200
 let score3 = 300
-
 console.log(Array.of(score1, score2, score3));
+//Array(3) -> create empty array of length 3
+//Array.of(100) -> [100]
